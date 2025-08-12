3dd17bd251393cc4719c6fe6f1469eaa91456aad
sebastian-VB <sebas99peru@gmail.com>
2025-08-12 17:15:10 -0500

    create aboutme section and add styles for aboutme info container

--- src/app/sections/aboutme/aboutme.component.ts
+++ src/app/sections/aboutme/aboutme.component.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SkillsComponent } from "../../components/skills/skills.component";
 
 @Component({
   selector: 'app-aboutme',
-  imports: [],
+  imports: [SkillsComponent],
   templateUrl: './aboutme.component.html',
   styleUrl: './aboutme.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutmeComponent { }
+export class AboutmeComponent { 
+
+  public greeting: string = 'Hola, soy Sebastián. Gusto en conocerte.';
+  public info: string = 'Bachiller en Ingeniería Informática, cerca de 2 años de experiencia en el Desarrollo de Software, interviniendo en la creación de funcionalidades para aplicaciones web, además de apoyar en la certificación de soluciones realizadas en sistemas de una empresa bancaria. Apasionado por la tecnología, interesado en aprender y aportar nuevas soluciones para la optimización de tareas anteriormente manuales y lentas.';
+  
+}
