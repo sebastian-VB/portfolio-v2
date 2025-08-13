da169e36923f8e3b0d0f5bfe7c3622760f717153
sebastian-VB <sebas99peru@gmail.com>
2025-08-13 10:28:19 -0500

    add transition in navbar when scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./sections/home/home.component";
+import { AboutmeComponent } from "./sections/aboutme/aboutme.component";
 
 @Component({
   selector: 'app-root',
   imports: [
-    HeaderComponent, 
-    HomeComponent
-  ],
+    HeaderComponent,
+    HomeComponent,
+    AboutmeComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, inject } from '@angular/core';
 import { NavBarService } from '../../services/navBar.service';
 
 @Component({
@@ -15,10 +15,18 @@ export class HeaderComponent {
   showMenu = false;
   hidingMenu = false;
 
+  isFixed = false;
+  isVisible = false;
+  isAtTop = true;
+
+  private threshold = 200;
+
   openMenu() {
     this.showMenu = true;
     this.hidingMenu = false;
     this.stateBtnService.setStateBtn(this.showMenu);
+
+    document.body.classList.add('no-scroll');
   }
 
   closeMenu() {
@@ -27,7 +35,33 @@ export class HeaderComponent {
       this.showMenu = false;
       this.hidingMenu = false;
       this.stateBtnService.setStateBtn(this.showMenu);
+
+      document.body.classList.remove('no-scroll');
     }, 600);
+  }
+
+  navigateAndClose(event: Event, targetId: string) {
+    event.preventDefault();
+    this.closeMenu();
+
+    setTimeout(() => {
+      const target = document.getElementById(targetId);
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' }); 
+      }
+    }, 600);
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollY > this.threshold) {
+      this.isFixed = true;
+      (scrollY > 300) ? this.isVisible = true : this.isVisible = false;
+    } else {
+      this.isFixed = false;
+    }
   }
 
 }
